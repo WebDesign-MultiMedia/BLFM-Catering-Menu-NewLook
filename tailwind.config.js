37deeef877ea6e70a03ee077c6e288a5b41ccba9
Julio <youremail@example.com>
2025-10-19 02:37:44 -0400

    tailwind color custome added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
         title: [ "Playwrite PL", "cursive"],
         head: [ "Playwrite PL",  "cursive"],
         menuFont: [ "Combo", "system-ui"]
-      }
+      },
+       colors:{
+      'bodyNavFooter': "#021828",
+      'greenMex': "#006341",
+      'whiteMex': "#ffffff",
+      'redMex' : "#CE1126"
+    }
     },
   },
   plugins: [],
