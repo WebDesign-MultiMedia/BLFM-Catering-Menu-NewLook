5f1cb553ea56a2ed8fb8eed70119f74afbbed1f1
WebDesign-MultiMedia <salasjulio386@gmail.com>
2025-11-30 02:03:19 -0500

    alert added

---
+++ src/alertPopup.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    Swal.fire({
+        title: "Hacemos Comida Para Eventos",
+        text: "Cumpleanos | Bautizo | 15s | Oficinas ",
+        showConfirmButton: false,
+        theme: 'dark',
+        timer: 4000,
+        timerProgressBar: true
+      });
+};
