ca94c678b6412e0243a7c16403fbac79dddeabae
WebDesign-MultiMedia <salasjulio386@gmail.com>
2025-12-06 17:25:36 -0500

    Performace updated

--- src/alertPopup.js
+++ src/alertPopup.js
@@ -1,10 +1,16 @@
-window.onload = function() {
+function showEventAlert() {
+  
     Swal.fire({
         title: "Hacemos Comida Para Eventos",
-        text: "Cumpleanos | Bautizo | 15s | Oficinas ",
+        text: "Perfect for birthday parties, baptisms, baby showers, office events and more. All dishes available by tray or by the pound.",
         showConfirmButton: false,
         theme: 'dark',
         timer: 4000,
-        timerProgressBar: true
-      });
+        timerProgressBar: true,
+        background: 'black',
+        backdrop: `
+        rgba(60, 60, 60, 0.90)`
+      })
 };
+
+
