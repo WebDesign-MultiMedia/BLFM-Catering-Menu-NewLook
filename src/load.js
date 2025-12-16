1f91aa944b5406da3dec5c85bc48f1e9fa2af574
WebDesign-MultiMedia <salasjulio386@gmail.com>
2025-12-16 02:37:59 -0500

    added svg image arroz blanco

--- src/load.js
+++ src/load.js
@@ -1,9 +1,9 @@
 
-const YTvids = document.getElementById('player');
+// const YTvids = document.getElementById('player');
 const mainEL = document.querySelector('main');
 
     // Element Hidden
-YTvids.style.visibility = 'hidden';
+// YTvids.style.visibility = 'hidden';
 mainEL.style.visibility = 'hidden';
 
     // Show popup agyer 2 seconds
@@ -12,7 +12,7 @@ mainEL.style.visibility = 'hidden';
 
          // Reveal the elements after 5 seconds 
            setTimeout(() => {
-            YTvids.style.visibility = 'visible';
+            // YTvids.style.visibility = 'visible';
             mainEL.style.visibility = 'visible';
           }, 5000);
         }, 0000);
